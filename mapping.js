b15326b41cea91f72bd607638313314f5353044f
Tom Chen <33406671+ToaToes@users.noreply.github.com>
2025-12-10 09:54:27 -0700

    Create mapping.js

---
+++ mapping.js
@@ -0,0 +1,24 @@
+// Class (舱位) mapping
+window.CLASS_MAP = {
+  economy: "经济",
+  premium_economy: "超级经济",
+  business: "商务",
+  first: "头等"
+};
+
+// Airline code → Chinese name mapping
+window.AIRLINE_MAP = {
+  AA: "美国航空",
+  AS: "阿拉斯加航空",
+  BA: "英国航空",
+  AF: "法国航空",
+  UA: "美联航",
+  AC: "加拿大航空",
+  CX: "国泰航空"
+};
+
+// Direct Flight
+window.DIRECT_MAP = {
+  true: "是",
+  false: "否"
+};
